20ba89023510b193c354aeff2902d7349d7f1ed0
sander541 <sandersven1@Gmail.com>
2018-02-17 20:31:08 +0200

    Navigeerimine

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
-  }
+
+	configureRouter(config, router) {
+        this.router = router;
+        config.title = 'Reisi application';
+        config.map([
+          { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
+          { route: 'Reisikogemused',            name: 'Reisikogemused',      moduleId: 'Reisikogemused', nav: true}      
+        ]);
+	}
 }
