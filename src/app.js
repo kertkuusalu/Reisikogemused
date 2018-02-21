006141c97c1563342989532f59f5008ad4c8da14
sander541 <sandersven1@gmail.com>
2018-02-21 13:58:25 +0200

    veebilehe navigate

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ export class App {
         config.title = 'Reisi application';
         config.map([
           { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
-          { route: 'Reisikogemused',            name: 'Reisikogemused',      moduleId: 'Reisikogemused', nav: true}      
+          { route: ['Reisikogemused'],            name: 'Reisikogemused',      moduleId: 'Reisikogemused', nav: true},
+          { route: ['Register'], name: 'Register',      moduleId: 'Register', nav: true},
+          { route: ['Login'],            name: 'Login',      moduleId: 'Login', nav: true}      
         ]);
 	}
 }
