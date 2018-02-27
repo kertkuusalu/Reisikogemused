aa93e8ab2d2851034f5414ce6768982e69f2ef6f
sander541 <sandersven1@gmail.com>
2018-02-27 16:08:01 +0200

    Add story interface

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ export class App {
           { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
           { route: ['Reisikogemused'],            name: 'Reisikogemused',      moduleId: 'Reisikogemused', nav: true},
           { route: ['Register'], name: 'Register',      moduleId: 'Register', nav: true},
-          { route: ['Login'],            name: 'Login',      moduleId: 'Login', nav: true}      
+          { route: ['Login'],            name: 'Login',      moduleId: 'Login', nav: true},
+		  { route: ['NewPost'],            name: 'NewPost',      moduleId: 'NewPost', nav: true}
         ]);
 	}
 }
