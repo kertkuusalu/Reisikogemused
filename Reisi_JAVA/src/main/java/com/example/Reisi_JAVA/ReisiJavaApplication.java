3ea5f4f9f35a3bf9d799fbb0bb06d4f516f5b470
sander541 <sandersven1@gmail.com>
2018-03-16 21:43:59 +0200

    gradle+spring finally töötab? parem organiseeritus

---
+++ Reisi_JAVA/src/main/java/com/example/Reisi_JAVA/ReisiJavaApplication.java
@@ -0,0 +1,12 @@
+package com.example.Reisi_JAVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReisiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReisiJavaApplication.class, args);
+	}
+}
