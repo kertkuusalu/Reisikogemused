08be4ad66809f844033ffdebfb8e0c5a71e433e2
sander541 <sandersven1@gmail.com>
2018-03-19 23:07:32 +0200

    Loeb postitused andmebaasist ja kuvab pealehel

--- ReisiPortal/src/Main/Reisikogemused.js
+++ ReisiPortal/src/Main/Reisikogemused.js
@@ -1,2 +1,18 @@
+import{HttpClient, json} from 'aurelia-fetch-client'
+
 export class Reisikogemused{
+	
+	postData = {}
+	postList = []
+	
+	activate() {
+		let client = new HttpClient();
+
+		client.fetch('http://localhost:8080/posts')
+			.then(response => response.json())
+			.then(posts => this.postList = posts);
+	}
+	
+	
+	
 }
