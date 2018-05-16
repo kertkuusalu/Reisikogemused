a9c3a5d806abdb2f17ce076dbb2c18f8f8d8d9c8
Aleksander Drozdov <36505072+aldroz22@users.noreply.github.com>
2018-05-16 11:24:06 +0300

    auth0 (delete)
    auht0 (delete)

--- ReisiPortal/src/app.js
+++ ReisiPortal/src/app.js
@@ -1,17 +1,7 @@
 import {RouterConfiguration, NavigationInstruction, Next, Redirect} from 'aurelia-router';
-import { PLATFORM } from 'aurelia-pal';
-import { inject } from 'aurelia-framework';
-import { AuthService } from './auth-service';
-@inject(AuthService)
 
 export class App {
 
-constructor(AuthService) {
-    this.auth = AuthService;
-    this.authenticated = this.auth.isAuthenticated();
-    this.auth.authNotifier.on('authChange', authState => {
-      this.authenticated = authState.authenticated;
-    });
   }
 	configureRouter(config, router) {
         this.router = router;
